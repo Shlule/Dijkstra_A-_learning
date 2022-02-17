8090b243113032deca7f7f21b6be2d15c80f5cdc
elouan rogliano <erogliano@artfx.fr>
2022-02-17 11:47:44 +0100

    finish pathfindlist class

--- Dijkstra_Astar_Learn/Dijkstra_Astar_Learn/PathFindingList.cpp
+++ Dijkstra_Astar_Learn/Dijkstra_Astar_Learn/PathFindingList.cpp
@@ -1,1 +1,31 @@
 #include "PathFindingList.h"
+using std::cout;
+
+void PathFindingList::AddNode(NodeRecord nodeP) {
+	nodes.emplace_back(nodeP);
+}
+
+void PathFindingList::RemoveNode(NodeRecord nodeP) {
+	if (nodes.size() != 0) {
+		for (int i = 0; i < nodes.size(); i++) {
+			if (nodes[i].getName() == nodeP.getName()) {
+				nodes.erase(nodes.begin() + i);
+			}
+		}
+	}
+	
+}
+
+NodeRecord PathFindingList::getSmallestElement() {
+	int smallerCost = 1000;
+	NodeRecord nodeToReturn;
+	if (nodes.size() != 0) {
+		for (int i = 0; i < nodes.size(); i++) {
+			if (nodes[i].getCostSoFar() < smallerCost) {
+				smallerCost = nodes[i].getCostSoFar();
+				nodeToReturn = nodes[i];
+			}
+		}
+	}
+
+}
--- Dijkstra_Astar_Learn/Dijkstra_Astar_Learn/PathFindingList.h
+++ Dijkstra_Astar_Learn/Dijkstra_Astar_Learn/PathFindingList.h
@@ -4,8 +4,8 @@
 class PathFindingList
 {
 public:
-	void AddNode();
-	void RemoveNode();
+	void AddNode(NodeRecord nodeP);
+	void RemoveNode(NodeRecord nodeP);
 	NodeRecord getSmallestElement();
 private:
 	std::vector<NodeRecord> nodes;
